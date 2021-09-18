0abd4100dd525f4239c6726225240c4bd8edb3b0
Mohamed Ali Sherif <mcit.alex85@gmail.com>
2021-09-18 21:15:00 +0200

    End of Section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
 import { IProduct } from 'src/app/shared/models/product';
 import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
@@ -11,7 +12,11 @@ import { ShopService } from '../shop.service';
 })
 export class ProductDetailsComponent implements OnInit {
 product :IProduct;
-  constructor(private shopService : ShopService, private activatedRoute : ActivatedRoute ,private bcService : BreadcrumbService) {
+quantity =1 ;
+  constructor(private shopService : ShopService,
+              private activatedRoute : ActivatedRoute ,
+              private bcService : BreadcrumbService,
+              private basketService :BasketService) {
 
     this.bcService.set('@productDetails',' ')
    }
@@ -19,6 +24,17 @@ product :IProduct;
   ngOnInit(): void {
     this.loadProduct();
   }
+addItemToBasket(){
+  this.basketService.addItemToBasket(this.product,this.quantity)
+}
+incrementQunatity(){
+
+  this.quantity++;
+}
+decrementQuantity(){
+  if(this.quantity>1)
+  this.quantity--;
+}
 
   loadProduct(){
   let id = Number(this.activatedRoute.snapshot.paramMap.get('id'));
